46a6ec8d7c820b85abf9ce1e9bb555fbb649de53
biancadmsadf16@gmail.com <biancadmsadf16@gmail.com>
2024-03-06 16:14:49 -0300

    agregando mas Jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,54 @@ var tooltipTriggerList = [].slice.call(
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
   });
-  // Tooltip: Incluir un botón "Enviar por correo" con un tooltip aplicado
-  
-  // $("#enviarCorreo").click(function () {
-  //   alert("El correo fue enviado correctamente...");
-  // });
-  // // cambiar de color al hacer doble click
-  // $(".ingredientes h3").on("dblclick", function () {
-  //   $(this).css("color", "red");
-  // });
   
   
-   // Seleccionar el elemento padre (div.card) y usar toggle para mostrar/ocultar
-  $('.card-title').click(function() {
+  $("#buttonEnviar").click(function () {
+    alert("Tu mensaje ha sido enviado");
+  });
+
+  $('.card').click(function() {
     $(this).closest('.card').find('.card-text').toggle();
+  });
+
+  
+  $(document).ready(function() {
+    // Manejador de eventos para cuando el mouse entra en el texto
+    $("#inicio").mouseenter(function() {
+      // Aplica la animación de salto
+      $(this).css("transform", "translateY(-10px)");
+    });
+    $("#inicio").mouseleave(function() {
+      // Reinicia la posición original
+      $(this).css("transform", "translateY(0)");
+    });
+  });
+
+  
+
+  $(document).ready(function() {
+    $("#QuienesSomos").mouseenter(function() {
+      $(this).css("transform", "translateY(-10px)");
+    });
+    $("#QuienesSomos").mouseleave(function() { 
+      $(this).css("transform", "translateY(0)");
+    });
+  });
+
+  $(document).ready(function() {
+    $("#Destacados").mouseenter(function() {
+      $(this).css("transform", "translateY(-10px)");
+    });
+    $("#Destacados").mouseleave(function() { 
+      $(this).css("transform", "translateY(0)");
+    });
+  });
+
+  $(document).ready(function() {
+    $("#Contacto").mouseenter(function() {
+      $(this).css("transform", "translateY(-10px)");
+    });
+    $("#Contacto").mouseleave(function() { 
+      $(this).css("transform", "translateY(0)");
+    });
   });
