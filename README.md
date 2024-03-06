896db5b2ef76c84a11509c24ab8e6973998ccc02
biancadmsadf16@gmail.com <biancadmsadf16@gmail.com>
2024-03-06 17:39:21 -0300

    cambios css

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
 "# Viaje_Chile" 
-"# Viaje_Chile" 
-"# Viaje_Chile" 
+
+Modulo 2 Desafio Latam
+
+Full Stack Javascript
+Generacion 70
+Profesor:Rodrigo Andrés Vargas Herrera
+Profesor ayudante: Piero Buccioni Arriagada
+Alumno:Bianca Salcedo
+
+
+Prueba - Viajes Chile:
+
+Instrucciones:
+
+Para realizar esta prueba debes haber estudiado previamente todo el material
+disponible en el LMS correspondiente al módulo.
+Una vez terminada la prueba, sube el proyecto a GitHub con los requerimientos
+solicitados y agrega el link del repositorio en el LMS.
+Puntaje total: 10 puntos.
+
+Desarrollo prueba:
+La prueba se debe desarrollar de manera Individual.
+Para la realización de la prueba necesitarás apoyarte del archivo Apoyo
+Prueba - Viajes Chile.
+
+Capítulos:
+
+
+● Introducción a HTML.
+● Crear páginas con HTML.
+● Introducción a CSS.
+● Añadiendo CSS al proyecto.
+● Introducción a Bootstrap.
+● Integrando Bootstrap.
+● Usando Bootstrap para personalizar el proyecto.
+● Componentes avanzados de Bootstrap.
+● Introducción a JavaScript.
+● Operaciones con JavaScript.
+● Integrando jQuery.
+● Bootstrap JS.
+● Terminal y manejo de archivos.
+● Introducción a Git.
+● Introducción a Github.
+● Trabajando con Git y Github.
+
+
+La página debe contener las siguientes secciones:
+● Una barra de navegación fija en la parte superior de la pantalla, con el logo y links a
+diferentes secciones de la página, haciendo un smooth scroll.
+● Un carousel que muestre las imágenes destacadas del sitio.
+● Una sección de presentación, utilizando favicons y 3 párrafos (debe desaparecer en
+tamaños pequeños de pantalla).
+● Una sección de destacados, que muestre 4 cards (tarjetas) con la imagen e
+información asociada.
+● Una sección de formulario de contacto.
+● Una sección footer con links a las redes sociales.
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,53 +6,37 @@ var tooltipTriggerList = [].slice.call(
     return new bootstrap.Tooltip(tooltipTriggerEl);
   });
   
-  
+  // Enviar alert al presionar el botón enviar
   $("#buttonEnviar").click(function () {
     alert("Tu mensaje ha sido enviado");
   });
 
+
+// Desaparecer el texto
   $('.card').click(function() {
     $(this).closest('.card').find('.card-text').toggle();
   });
 
-  
+
+  // Agregando saltos de texto
   $(document).ready(function() {
     // Manejador de eventos para cuando el mouse entra en el texto
-    $("#inicio").mouseenter(function() {
+    $("#inicio, #QuienesSomos, #Destacados, #Contacto").mouseenter(function() {
       // Aplica la animación de salto
       $(this).css("transform", "translateY(-10px)");
     });
-    $("#inicio").mouseleave(function() {
+    $("#inicio, #QuienesSomos, #Destacados, #Contacto").mouseleave(function() {
       // Reinicia la posición original
       $(this).css("transform", "translateY(0)");
     });
   });
 
-  
 
+// Agregando Sombra en el texto del Navbar
   $(document).ready(function() {
-    $("#QuienesSomos").mouseenter(function() {
-      $(this).css("transform", "translateY(-10px)");
-    });
-    $("#QuienesSomos").mouseleave(function() { 
-      $(this).css("transform", "translateY(0)");
-    });
-  });
-
-  $(document).ready(function() {
-    $("#Destacados").mouseenter(function() {
-      $(this).css("transform", "translateY(-10px)");
-    });
-    $("#Destacados").mouseleave(function() { 
-      $(this).css("transform", "translateY(0)");
-    });
-  });
-
-  $(document).ready(function() {
-    $("#Contacto").mouseenter(function() {
-      $(this).css("transform", "translateY(-10px)");
-    });
-    $("#Contacto").mouseleave(function() { 
-      $(this).css("transform", "translateY(0)");
+    $("#inicio, #QuienesSomos, #Destacados, #Contacto").on('mouseenter', function() {
+      $(this).css('text-shadow', '2px 32px 4px rgba(0, 0, 0, 0.9)');
+    }).on('mouseleave', function() {
+      $(this).css('text-shadow', 'none');
     });
   });
